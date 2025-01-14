79a50e6f06334e2684481dad62bf52a0d9ec6ae7
Charlie Hodgkinson <charlotte.hodgkinson4@gmail.com>
2025-01-14 21:35:20 +0000

    intital note/ irl crush app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,141 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import {
+  Authenticator,
+  Button,
+  TextField,
+  Heading,
+  Flex,
+  View,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
 
-function App() {
+Amplify.configure(outputs);
+const client = generateClient({
+  authMode: "userPool",
+});
+
+export default function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const { data: crushSession } = await client.models.crushSession.list();
+    await Promise.all(
+      crushSession.map(async (note) => {
+        return note;
+      })
+    );
+    console.log(crushSession);
+    setNotes(crushSession);
+  }
+
+  async function createNote(event) {
+    event.preventDefault();
+    const form = new FormData(event.target);
+
+    const { data: newCrushSession } = await client.models.crushSession.create({
+      name: form.get("name"),
+    });
+
+    console.log(newCrushSession);
+
+    fetchNotes();
+    event.target.reset();
+  }
+
+  async function deleteNote({ id }) {
+    const toBeDeletedCrushSession = {
+      id: id,
+    };
+
+    const { data: deletedCrushSession } = await client.models.crushSession.delete(
+      toBeDeletedCrushSession
+    );
+    console.log(deletedCrushSession);
+
+    fetchNotes();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-
-        <h1>Hello from Amplify</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut }) => (
+        <Flex
+          className="App"
+          justifyContent="center"
+          alignItems="center"
+          direction="column"
+          width="70%"
+          margin="0 auto"
+        >
+          <Heading level={1}>IRL Crush</Heading>
+          <View as="form" margin="3rem 0" onSubmit={createNote}>
+            <Flex
+              direction="column"
+              justifyContent="center"
+              gap="2rem"
+              padding="2rem"
+            >
+              <TextField
+                name="name"
+                placeholder="Event name"
+                label="Event name"
+                labelHidden
+                variation="quiet"
+                required
+              />
+            
+              <Button type="submit" variation="primary">
+                Start new IRL Crush event
+              </Button>
+            </Flex>
+          </View>
+          <Divider />
+          <Heading level={2}>Current IRL Crush events</Heading>
+          <Grid
+            margin="3rem 0"
+            autoFlow="column"
+            justifyContent="center"
+            gap="2rem"
+            alignContent="center"
+          >
+            {notes.map((note) => (
+              <Flex
+                key={note.id || note.name}
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                gap="2rem"
+                border="1px solid #ccc"
+                padding="2rem"
+                borderRadius="5%"
+                className="box"
+              >
+                <View>
+                  <Heading level="3">{note.name}</Heading>
+                </View>
+                <Button
+                  variation="destructive"
+                  onClick={() => deleteNote(note)}
+                >
+                  Delete note
+                </Button>
+              </Flex>
+            ))}
+          </Grid>
+          <Button onClick={signOut}>Sign Out</Button>
+        </Flex>
+      )}
+    </Authenticator>
   );
 }
-
-export default App;
